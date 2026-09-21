7205d736de8cd0d39291724cdbbb91202bb1d8fd
Theo Ferreira <theo.ferreira@Alpha.Cloud>
2026-09-21 16:20:39 -0300

    Restore files accidentally dropped from the previous commit

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  server: {
+    cors: {
+      origin: "https://www.owlbear.rodeo",
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        background: resolve(__dirname, "background.html"),
+      },
+    },
+  },
+});
